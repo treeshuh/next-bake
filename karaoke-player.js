5411535b0e60ceaee002bc92a9083e8266117f8c
treeshuh <tricia.shi@gmail.com>
2016-01-05 19:51:02 -0600

    sessions

---
+++ karaoke-player.js
@@ -0,0 +1,120 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+    var browserKey = "AIzaSyD3Ivd-nft7gnXM35ldEtI-FO9YS3RYi5I";
+    var fb = new Firebase("https://next-bake.firebaseio.com/");
+    var songs = [];
+    var sessionID = "placeholder";
+    var currentSongNum = 0;
+    var currentSong = null;
+    var currentIndex = 0;
+    var me = this;
+    fb.child('karaoke').child('activeSessions').on("child_added", function(snapshot){
+      option = document.createElement("option");
+      console.log(snapshot.val());
+      addedSession = snapshot.val();
+      date = new Date(addedSession.date);
+      option.text = addedSession.group + " " + addedSession.ID + " " + date.toLocaleDateString();
+      option.val = addedSession.ID;
+      document.getElementById("sessionList").appendChild(option);
+    });
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+      //http://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript
+      var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+      var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+      player = new YT.Player('player', {
+        height: h,
+        width: w,
+        videoId: 'M7lc1UVf-VE',
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
+
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+      window.alert("I'm ready!");
+    }
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    var done = false;
+    function onPlayerStateChange(event) {
+      if (event.data == 0) {
+        console.log("donezo");
+        nextSong();
+      }
+    }
+
+    function stopVideo() {
+      player.stopVideo();
+    }
+
+    window.onresize = function() {
+      var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+      var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+      player.setSize(w, h);
+    }
+
+    function nextSong() {
+      currentSongNum += 1;
+      currentIndex = 0;
+        if (currentSongNum < songs.length) {
+          playVid();
+        } else {
+          console.log("end of playlist!");
+        }
+    }
+
+    function nextVidID() {
+      currentIndex += 1;
+      if (currentIndex < currentSong.videoIDs.length) {
+        playVid();     
+      } else {
+        console.log("out of vidID options!");
+      }
+    }
+
+    function jumpToQueue(num) {
+      currentSongNum = num;
+      currentIndex = 0;
+      playVid();
+    }
+
+    function playVid() {
+      currentSong = songs[currentSongNum];
+      vidID = currentSong.videoIDs[currentIndex];
+      player.loadVideoById(vidID);
+      player.playVideo();   
+    }
+
+    function newSession() {
+      queueItem = fb.child('karaoke').child('activeSessions').push();
+      queueItem.set({
+        group: document.getElementById("newSessionGroup").value,
+        date: Date.now(),
+        ID: document.getElementById("newSessionName").value
+      })
+    }
+
+    function changeSession() {
+      e = document.getElementById("sessionList");
+      val = e.options[e.selectedIndex].value;
+      sessionID = val;
+      songs = [];
+      fb.child('karaoke').child(sessionID).on("child_added", function(snapshot) {
+        console.log(snapshot.val());
+        songs.push(snapshot.val());
+        console.log(songs)
+      });
+    }
--- karaoke.js
+++ karaoke.js
@@ -1,60 +1,7 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-    var browserKey = "AIzaSyD3Ivd-nft7gnXM35ldEtI-FO9YS3RYi5I";
-    var fb = new Firebase("https://next-bake.firebaseio.com/");
-    var songs = [];
-    var currentSongNum = 0;
-    var currentSong = null;
-    var me = this;
-    fb.child('karaoke').child('session1').on("child_added", function(snapshot) {
-      console.log(snapshot.val());
-      songs.push(snapshot.val());
-      console.log(songs)
-    });
 
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'M7lc1UVf-VE',
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
-    }
-
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-      currentSong = songs[currentSongNum];
-      vidID = currentSong.videoIDs[0];
-      player.loadVideoById(vidID);
-      event.target.playVideo();
-    }
-
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-      if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-      }
-      if (event.data == 0) {
-        console.log("donezo");
-      }
-    }
-    function stopVideo() {
-      player.stopVideo();
-    }
+  var fb = new Firebase("https://next-bake.firebaseio.com/");
+      var browserKey = "AIzaSyD3Ivd-nft7gnXM35ldEtI-FO9YS3RYi5I";
+      sessionID = "placeholder";
 $(document).on('ready', function(){
 
   $("#search").on('click',function() {
@@ -62,7 +9,6 @@ $(document).on('ready', function(){
     songName = $("#songName").val();
     songArtist = $("#songArtist").val();
     searchText = songName + "+" + songArtist + "+lyrics";
-    console.log(searchText);
 
     $.ajax({
       url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchText.replace(" ", "+") + "&key=" + browserKey
@@ -73,20 +19,12 @@ $(document).on('ready', function(){
 
       for (i = 0; i < n; i ++) {
         item = data.items[i];
-        /*if (item.id.kind == "youtube#video") {
-          $("#searchResults").append("<div>" +
-            "<img src="+item.snippet.thumbnails.default.url+"/>" +
-            "<label>Title</label>" + item.snippet.title +
-            "<label>Channel</label>" + item.snippet.channelTitle + 
-            "<label>Date</label>" + new Date(item.snippet.publishedAt)+
-            "</div>");
-        }*/
         if (item.id.kind=="youtube#video") {
           videoIDs.push(item.id.videoId);
         }
       }
       console.log(videoIDs);
-      newQueueItem = fb.child("karaoke").child("session1").push();
+      newQueueItem = fb.child("karaoke").child(sessionID).push();
       newQueueItem.set({
         person: userText,
         song: {
@@ -95,6 +33,28 @@ $(document).on('ready', function(){
         },
         videoIDs: videoIDs
       });
-    })
-  })
+    });
+  });  
+
 })
+fb.child('karaoke').child('activeSessions').on("child_added", function(snapshot){
+      option = document.createElement("option");
+      console.log(snapshot.val());
+      newSession = snapshot.val();
+      date = new Date(newSession.date);
+      option.text = newSession.group + " " + newSession.ID + " " + date.toLocaleDateString();
+      option.val = newSession.ID;
+      document.getElementById("sessionList").appendChild(option);
+    });
+
+    function changeSession() {
+      e = document.getElementById("sessionList");
+      val = e.options[e.selectedIndex].value;
+      sessionID = val;
+      songs = [];
+      fb.child('karaoke').child(sessionID).on("child_added", function(snapshot) {
+        console.log(snapshot.val());
+        songs.push(snapshot.val());
+        console.log(songs)
+      });
+    }
